e0ab96b729409d8dbbef95c89d5b1c3015caeec5
harshithareddy123456 <vsharshi233@gmail.com>
2024-06-14 11:05:08 +0530

    date picker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react";
 function App() {
+  const [selecteddate, setSelecteddate] = useState(null);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DatePicker
+        selected={selecteddate}
+        onChange={(date) => setSelecteddate(date)}
+        placeholderText="Select a date"
+        dateFormat="dd/MM/yyyy"
+        minDate={new Date()}
+        filterDate={(date) => date.getDay() !== 6 && date.getDay() !== 0}
+        isClearable
+        showYearDropdown
+        scrollableMonthYearDropdown
+      />
     </div>
   );
 }
